c17027666560c2511049d63bebf71a47c5b9af65
Anders Sundheim <andersundheim@gmail.com>
2021-06-12 15:51:54 -0500

    release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cox-dry-streak';
+  file : any;
+  dryStreak = 0;
+  dryPoints : number[] = [];
+  dryStreaks : any[] = [];
+
+  constructor() {
+    
+  }
+
+  fileBrowserHandler(files: any) {
+    this.file = files.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      const result : any = fileReader.result;
+      console.log(result);
+      const data = result.split("\n").filter((x : any) => x.length > 10).map((x : any) => {
+        let result = JSON.parse(x);
+        console.log(result);
+        return result;
+      });
+      console.log(data);
+      //;
+      data.forEach((i : any) => {
+        this.dryStreak++;
+        this.dryPoints.push(i.totalPoints);
+        if (i.specialLoot) {
+            let prob = 1 - this.dryPoints.map(x => 1 - this.chance(x)).reduce((a,b) => a * b, 1);
+            this.dryStreaks.push({streak: this.dryStreak, loot: i.specialLoot, receiver: i.specialLootReceiver, p: prob, points: this.totalPoints()});
+            console.log(`Dry Streak: ${this.dryStreak} - broken by ${i.specialLoot} from ${i.specialLootReceiver} - probability of purple ${prob * 100}% (${this.totalPoints()} points)`);
+            this.dryStreak = 0;
+            this.dryPoints = [];
+        }
+      });
+      let prob = 1 - this.dryPoints.map(x => 1 - this.chance(x)).reduce((a,b) => a * b, 1);
+      this.dryStreaks.push({streak: this.dryStreak, loot: null, receiver: null, p: prob, points: this.totalPoints()})
+      console.log(`Current Dry Streak: ${this.dryStreak} - probability of purple ${prob * 100}% (${this.totalPoints()} points)`);
+    }
+    fileReader.readAsText(this.file);
+  }
+
+  factorial(num: any){
+      var rval=1;
+      for (var i = 2; i <= num; i++)
+          rval = rval * i;
+      return rval;
+  }
+
+  chance(num : any){
+      return ( num / 570000 ) * .8;
+  }
+
+  totalPoints(){
+      return this.dryPoints.reduce((a,b) => a + b, 0);
+  }
+
+  streakToString(streak: any) {
+    return `Dry Streak: ${streak.streak} - broken by ${streak.loot} from ${streak.receiver} - probability of purple ${streak.p}% (${streak.points} points)`;
+  }
 }
